58f5aa7b0aed4a532c8db92aa5cdd5ee7e3f0648
Motlaq-Alnassafi <alnassafimotlaq@gmail.com>
2025-02-18 17:36:17 +0300

    Motlaq Alnesafi

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
-    println("Hello World!")
+    var age: Int? = null
+    var cityName: String? = "Kuwait City"
+    cityName = null
+    println(cityName?.length?: 5)
+    score(50.2)
+    score(null)
+}
+
+fun score(score: Double?) {
+    score?.let { nullSafe ->
+        println(nullSafe)
+    }?: println(0.0)
 }
